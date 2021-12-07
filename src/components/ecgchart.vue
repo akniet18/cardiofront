8aa3b65723cbb4a602761f077e3709f807fb20bb
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-12-07 20:52:19 +0600

    Commit

--- src/components/ecgchart.vue
+++ src/components/ecgchart.vue
@@ -164,8 +164,8 @@ export default {
           // let mmax = self.series.getYMax() + 100000
           // let mmin = self.series.getYMin() - 100000
           // if (Math.round(old - d[i]) > 2000){
-          let mmax = d[i] + 50000;
-          let mmin = d[i] - 50000;
+          let mmax = d[i] + 55000;
+          let mmin = d[i] - 55000;
           self.chart
             .getDefaultAxisY()
             .setTickStrategy("Empty")
